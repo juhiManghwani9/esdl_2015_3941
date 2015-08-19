dc04a3d58824f798afb6203dffbd3775b14e6ac0
juhiManghwani9 <manghwanijuhi@gmail.com>
2015-08-19 14:32:25 +0530

    updated in replica branch

--- calc.cpp
+++ calc.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 int main()
 {
-  int a,b,c;
+  int a,b,c,d;
   cout<<"Enter two nos: "
   cin>>a>>b;
   c=a+b;
   cout<<"Addition of two nos: "<<c;
+  d=a-b;
+  cout<<"Subtration of two nos: "<<d;
   return 0;
 }
